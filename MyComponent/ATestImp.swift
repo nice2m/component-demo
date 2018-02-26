1ad9a62839c2cdbf13ba685666b990a76628d767
xiedacai <xiedacai@vcredit.com>
2018-02-26 16:12:02 +0800

    推送测试提交

---
+++ MyComponent/ATestImp.swift
@@ -0,0 +1,14 @@
+//
+//  ATestImp.swift
+//  Component-Test
+//
+//  Created by HuanLeiMac on 2018/2/26.
+//  Copyright © 2018年 VisionMicrocreditCo. All rights reserved.
+//
+
+import UIKit
+
+class ATestImp: NSObject {
+
+    
+}
